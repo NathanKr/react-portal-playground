52db718499195361253188aae6fdb2f0a2da3e85
Nathan Krasney <natankrasney@gmail.com>
2021-12-28 16:08:13 +0200

    initial version

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# react-portal-playground
+<h2>Motivation</h2>
+Understand react portal
+
+
+
+
+
+<h2>Credits</h2>
+This is based on <a href='https://codepen.io/gaearon/pen/yzMaBd'>Dan Abramov code</a>
---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import './App.css'
+import Modal from './components/Modal'
+
+
+// The Modal component is a normal React component, so we can
+// render it wherever we like without needing to know that it's
+// implemented with portals.
+class App extends React.Component {
+  state = {showModal: false}
+
+  handleShow = () => {
+    this.setState({showModal: true});
+  }
+  
+  handleHide = () => {
+    this.setState({showModal: false});
+  }
+
+  render() {
+    // Show a Modal on click.
+    // (In a real app, don't forget to use ARIA attributes
+    // for accessibility!)
+    const modal = this.state.showModal ? (
+      <Modal>
+        <div className="modal">
+          <div>
+            With a portal, we can render content into a different
+            part of the DOM, as if it were any other React child.
+          </div>
+          This is being rendered inside the modal container div : modal-root.
+          <button onClick={this.handleHide}>Hide modal</button>
+        </div>
+      </Modal>
+    ) : null;
+
+    return (
+      <div className="app">
+        This div has overflow: hidden.
+        <button onClick={this.handleShow}>Show modal</button>
+        {modal}
+      </div>
+    );
+  }
+}
+
+export default App;
